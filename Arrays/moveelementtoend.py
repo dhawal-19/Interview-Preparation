4c3caab8e6253dc6ec46396742224c190f956b01
Dhawal Kumar Singh <dhawal.singh10@gmail.com>
2021-03-29 04:03:44 +0530

    Move Element To End Added

---
+++ Arrays/moveelementtoend.py
@@ -0,0 +1,16 @@
+import sys
+def get_ints(): return list(map(int, sys.stdin.readline().strip().split()))
+
+
+array = get_ints()
+target = int(input())
+
+i = 0
+j = len(array)-1
+while i < j:
+    while i < j and array[j] == target:
+        j -= 1
+    if array[i] == target:
+        array[i], array[j] = array[j], array[i]
+    i += 1
+print(array)
