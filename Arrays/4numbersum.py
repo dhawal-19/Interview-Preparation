3316353d80c846f812ad49a4a2bd6a49614a65ab
Dhawal Kumar Singh <dhawal.singh10@gmail.com>
2021-04-06 01:25:23 +0530

    Four Number Sum + Subarray Sort Completed

---
+++ Arrays/4numbersum.py
@@ -0,0 +1,27 @@
+import sys
+def get_ints(): return list(map(int, sys.stdin.readline().strip().split()))
+
+
+array = get_ints()
+print(array)
+
+target = int(input())
+
+allPairSums = {}
+
+quadruplets = []
+for i in range(1,len(array)-1):
+    for j in range(i+1,len(array)):
+        currentSum = array[i] + array[j]
+        differenceSum = target - currentSum
+        if differenceSum in allPairSums:
+            for pair in allPairSums[differenceSum]:
+                quadruplets.append(pair+[array[i], array[j]])
+
+    for k in range(0, i):
+        currentSum = array[i] + array[k]
+        if currentSum not in allPairSums:
+            allPairSums[currentSum] = [[array[i], array[k]]]
+        else:
+            allPairSums[currentSum].append([array[i], array[k]])
+print(quadruplets)
---
+++ Arrays/subarraysort.py
@@ -0,0 +1,37 @@
+import sys
+
+
+def get_ints(): return list(map(int, sys.stdin.readline().strip().split()))
+
+
+array = get_ints()
+print(array)
+
+minOutOfOrder = float("inf")
+maxOutOfOrder = float("-inf")
+
+
+def isoutoforder(i, num, array):
+    if i == 0:
+        return num > array[i + 1]
+    if i == len(array) - 1:
+        return num < array[i - 1]
+    return num > array[i + 1] or num < array[i - 1]
+
+
+for i in range(len(array)):
+    num = array[i]
+    if isoutoforder(i, num, array):
+        minOutOfOrder = min(minOutOfOrder, num)
+        maxOutOfOrder = max(maxOutOfOrder, num)
+
+if minOutOfOrder == float("inf"):
+    print([-1, -1])
+    exit(0)
+subArrayLeftIdx = 0
+while 0 <= subArrayLeftIdx <= len(array) - 1 and minOutOfOrder >= array[subArrayLeftIdx]:
+    subArrayLeftIdx += 1
+subArrayRightIdx = len(array) - 1
+while 0 <= subArrayRightIdx <= len(array) - 1 and maxOutOfOrder <= array[subArrayRightIdx]:
+    subArrayRightIdx -= 1
+print([subArrayLeftIdx, subArrayRightIdx])
